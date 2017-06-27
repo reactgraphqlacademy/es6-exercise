82335de46dd5b1c17c42939611323b8803f1c524
alexlbr <alex.lobera@gmail.com>
2017-06-27 13:06:25 +0100

    styled test 04

--- src/04_spread-operator.test.js
+++ src/04_spread-operator.test.js
@@ -5,12 +5,14 @@ describe('spread with arrays', () => {
 
   it('extracts each array item', () => {
     const [b, a] = [...[1, 2]]
+
     expect(a).toEqual(1)
     expect(b).toEqual(2)
   })
 
   it('in combination with rest', () => {
     const [a, b, ...rest] = [...[0, 1, 2, 3, 4, 5]]
+
     expect(a).toEqual(1)
     expect(b).toEqual(2)
     expect(rest).toEqual([3, 4, 5])
@@ -18,6 +20,7 @@ describe('spread with arrays', () => {
 
   it('spreading into the rest', () => {
     const [...rest] = [...[,1, 2, 3, 4, 5]]
+
     expect(rest).toEqual([1, 2, 3, 4, 5])
   })
 
@@ -25,6 +28,7 @@ describe('spread with arrays', () => {
     it('prefix with `...` to spread as function params', () => {
       const magicNumbers = [1, 2]
       const fn = (magicA, magicB) => {
+
         expect(magicNumbers[0]).toEqual(magicA)
         expect(magicNumbers[1]).toEqual(magicB)
       }
@@ -41,28 +45,33 @@ describe('spread with strings', () => {
 
   it('simply spread each char of a string', () => {
     const [b, a] = [...'ab']
+
     expect(a).toEqual('a')
     expect(b).toEqual('b')
   })
 
   it('extracts each array item', () => {
     const [a,,c] = ['a', ...'12']
+
     expect(a).toEqual('1')
     expect(b).toEqual('2')
   })
 
   it('works anywhere inside an array (must not be last)', () => {
     const letters = ['a', 'bcd', 'e', 'f']
+
     expect(letters.length).toEqual(6)
   })
 
   it('dont confuse with the rest operator', () => {
     const [...rest] = ['1234', ...'5']
+
     expect(rest).toEqual(['1', '2', '3', '4', '5'])
   })
 
   it('passed as function parameter', () => {
     const max = Math.max(12345)
+
     expect(max).toEqual(5)
   })
 
