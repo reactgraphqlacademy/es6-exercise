10f9f603bd3a82b851b510d41480205396b11070
alexlbr <alex.lobera@gmail.com>
2018-12-03 12:57:34 +0000

    src/06_arrow-function.test.js

--- src/06_arrow-function.test.js
+++ src/06_arrow-function.test.js
@@ -1,23 +1,23 @@
 // 06: arrow functions
 
-describe('06 Arrow functions', () => {
-  it('Should bind the context to this', () => {
+describe("06 Arrow functions", () => {
+  it("Should bind the context to this", () => {
     const person = {
       age: 1,
 
-      add: function (age) {
+      add: function(age) {
         // finish the implementation of the next function using an arrow function
         // and using the parameter v
         function f(v) {
-          return thils.age
+          return this.age + v;
         }
 
-        return f(age)
-      },
-    }
+        return f(age);
+      }
+    };
 
-    let actual = person.add(1)
+    let actual = person.add(1);
 
-    expect(actual).toEqual(2)
-  })
-})
+    expect(actual).toEqual(2);
+  });
+});
